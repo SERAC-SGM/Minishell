45949bca6a8cc9778cb3e48e955fc718ab6d95ea
lletourn <lletourn@student.42.fr>
2023-05-04 14:25:46 +0200

    parser

--- srcs/02_parser/parser.c
+++ srcs/02_parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: lletourn <lletourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 14:19:32 by maaliber          #+#    #+#             */
-/*   Updated: 2023/05/04 14:19:55 by lletourn         ###   ########.fr       */
+/*   Updated: 2023/05/04 14:25:00 by lletourn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 t_data	parser(t_data *data, t_tkn_lst cmd_line)
 {
-/*
-Si !content -> special character => check type
-*/
 while (cmd_line)
 {
-	if (cmd_line->con)
+	if (!cmd_line->content)
+	{
+		
+	}
 }
 }
