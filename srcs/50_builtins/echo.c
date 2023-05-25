57a499369db3584472604530aa1e2510b1ba7aa1
lletourn <lletourn@student.42.fr>
2023-05-25 11:24:40 +0200

    fixed echo builtin

--- srcs/50_builtins/echo.c
+++ srcs/50_builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: lletourn <lletourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 11:10:57 by lletourn          #+#    #+#             */
-/*   Updated: 2023/05/24 12:25:44 by lletourn         ###   ########.fr       */
+/*   Updated: 2023/05/25 11:22:20 by lletourn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,25 @@
 
 int	ft_echo(char **args)
 {
-	int	n_option;
+	int	end_newline;
 	int	i;
 
-	n_option = 1;
+	end_newline = 1;
 	if (!args[1])
 		return (0);
-	i = 1;
-	while (args[i] && ft_strlen(args[i]) == ft_strlen("-n")
-		&& !ft_strncmp("-n", args[i], ft_strlen("-n")))
+	i = 0;
+	while (args[++i] && args[i][0] == '-')
 	{
-		n_option = 0;
-		i++;
+		if (args[i][1] == 'n')
+			end_newline = 0;
 	}
 	while (args[i])
 	{
 		ft_putstr_fd(args[i++], 1);
-		if (args[i + 1])
+		if (args[i])
 			write(1, " ", 1);
 	}
-	if (n_option)
+	if (end_newline)
 		write(1, "\n", 1);
 	return (0);
 }
