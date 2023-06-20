0812cc0457026b7e14d362db7e91c93eb4705110
lletourn <lletourn@student.42.fr>
2023-06-20 14:48:33 +0200

    push pour faire semblant que je taffe

--- srcs/00_core/minishell.c
+++ srcs/00_core/minishell.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maaliber <maaliber@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lletourn <lletourn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 14:58:49 by maaliber          #+#    #+#             */
-/*   Updated: 2023/06/19 16:36:44 by maaliber         ###   ########.fr       */
+/*   Updated: 2023/06/20 14:07:28 by lletourn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// static void	display_prompt(t_data *data)
+// {
+// 	char	cwd[BUFFER_SIZE];
+// 	char	*prompt;
+
+// 	prompt = ft_strjoin(getcwd(cwd, BUFFER_SIZE), "$ ");
+// 	data->cmd_line = readline(prompt);
+// 	free(prompt);
+// }
 
 int	main(int ac, char *av[], char *env[])
 {
@@ -24,6 +34,7 @@ int	main(int ac, char *av[], char *env[])
 	{
 		reset_data(&data);
 		data.cmd_line = readline("minishell$ ");
+		//display_prompt(&data);
 		if (!data.cmd_line)
 			break ;
 		if (*data.cmd_line)
